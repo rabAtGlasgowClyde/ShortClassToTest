8f8e18daaaf8a521070112b8d9130cbd44b4a9f3
Robert Henderson <rhenderson@glasgowclyde.ac.uk>
2017-02-02 15:33:50 +0000

    TestClass for Practice Assignment
    Test Class for Practice Assignment - Please note you must declare your class under test as "public" otherwise the method you wish to test may not be visible to the test class.

---
+++ findHighestTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using findTheHighest;
+namespace findHighestTest
+{
+    [TestClass]
+    public class findHighestTest
+    {
+        [TestMethod]
+        public void Test_findHighest()
+        {  //Arrange
+            int expected = 10;
+            int [] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+           int actual =  HighestClass.findHighest(nums);
+           Assert.AreEqual(expected, actual);
+           
+            
+        }
+    }
+}
