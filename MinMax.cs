1e9e57b2e724def4c0ed887f2711be0d76f5cf02
Robert Henderson <rhenderson@glasgowclyde.ac.uk>
2017-02-02 13:28:49 +0000

    findHighest Algorithm
    findHighest Algorithm coded in C# to practice testing

---
+++ MinMax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMaxProject
+{
+    class MinMax
+    {
+
+        public int findHighest(int[] anArray) // method to find the student with the highest mark
+        {
+            int Max = anArray[0]; // set first index to max
+            for (int i = 0; i < anArray.Length; i++ ) // iterate over array
+            {
+                if (anArray[i] > Max) // check current number is greater than max
+                {
+
+                    Max = anArray[i]; // swap if greater is true
+
+                }
+
+
+            }
+
+
+
+
+            return Max;
+        }
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
